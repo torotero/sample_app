088a1fc0c23fc153ce607235e0aa7c40b0f3d287
T_Ro <ganza.toro@gmail.com>
2015-03-01 04:35:51 +0700

    Add helper full title and refactoring

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+
+	def setup
+	@base = "Ruby on Rails Tutorial Sample App"
+	end
+
+
+
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "#{@base}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base}"
   end
 
 
   test "about pages harus ada" do 
   	get :about
   	assert_response :success
-  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  	assert_select "title", "About | #{@base}"
   end
 
 end
